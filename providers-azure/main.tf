c8cb8f4b86b489739cb713415e73f865d2fe86ad
Guillermo Celarie <lgcelarie@Guillermos-MacBook-Pro.local>
2023-02-12 20:49:26 -0600

    Basic azure provider usage

--- providers-azure/main.tf
+++ providers-azure/main.tf
@@ -22,6 +22,22 @@ module "linuxservers" {
   vm_os_simple        = "UbuntuServer"
   public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
   vnet_subnet_id      = module.network.vnet_subnets[0]
+  vm_size             = "Standard_B1ls"
 
   depends_on = [azurerm_resource_group.testgroup]
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.testgroup.name
+  subnet_prefixes     = ["10.230.0.0/24"]
+  subnet_names        = ["subnet1"]
+  use_for_each = false
+  
+
+  depends_on = [azurerm_resource_group.testgroup]
+}
+
+output "linux_vm_public_name" {
+  value = module.linuxservers.public_ip_dns_name
 }
