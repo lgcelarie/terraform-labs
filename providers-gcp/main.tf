4052a8aaeefda3747bdeff1664931d62b2069579
Guillermo <lg.celarie@gmail.com>
2023-02-14 20:23:54 -0600

    basic usage of provider to create a compute resource

--- providers-gcp/main.tf
+++ providers-gcp/main.tf
@@ -1,15 +1,40 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.52.0"
     }
   }
 }
 
 provider "google" {
- zone = "us-central1-c"
- region = "us-central1"
- credentials = "appid.json"
- project = "idgoeshere"
+  zone        = "us-central1-c"
+  region      = "us-central1"
+  credentials = "appid.json"
+  project     = "idgoeshere"
+}
+
+resource "google_compute_instance" "compute_test" {
+  name         = "test"
+  machine_type = "e2-medium"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
 }
