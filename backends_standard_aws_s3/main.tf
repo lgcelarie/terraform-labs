8d6d97d76c9a4c4d503ce9f29117767da0d8de7e
Guillermo <lg.celarie@gmail.com>
2023-03-04 14:56:46 -0600

    adding resource to be registered in the state

--- backends_standard_aws_s3/main.tf
+++ backends_standard_aws_s3/main.tf
@@ -5,3 +5,51 @@ terraform {
     region = "us-east-1"
   }
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block       = "11.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "TerraformVPC"
+  }
+}
+
+resource "aws_subnet" "terraform_subnet" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = "11.0.0.0/24"
+
+  tags = {
+    Name = "TerraformSubnet"
+  }
+}
+
+resource "aws_instance" "my_instance" {
+  ami                         = "ami-0b5eea76982371e91"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.terraform_subnet.id
+
+  tags = {
+    Name = "Server-Terraform"
+  }
+}
+
+
+output "public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
